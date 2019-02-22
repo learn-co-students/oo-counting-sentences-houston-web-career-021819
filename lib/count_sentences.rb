c53d14ee96b8c5fd9faecb0312aba26862c62ba2
HemalathaMurugan <hema@Hemalathas-MBP.attlocal.net>
2019-02-22 00:56:21 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
 
+    temp = self.split(/[.!?...]/)
+    temp.delete_if do |word|
+      word.empty?
+    end
+    temp.count
   end
+
 end
