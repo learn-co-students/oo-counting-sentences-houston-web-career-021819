302a35a8852b376a00bc4639e159a2b24bc7fc01
JingChen <jingchenjc2019@gmail.com>
2019-02-21 18:15:11 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,37 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
+      #use regular expression, | is either or
+      #\. =. \? =? \! =!
+      #+ :  One or more times, if the sentence contains 1 or more punctuation marks
+      self.split(/\.+|\?+|\!+/).length
+
+      #edge case: if the sentence only has a word, a word shorter than 2 letters cannnot be a sentence
+      # return newarray.delete_if?{|word| word.length<2}
 
   end
 end
